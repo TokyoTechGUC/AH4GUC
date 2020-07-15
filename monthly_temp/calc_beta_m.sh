c3e30e457abf1e0b035573689d98155a0ae3af79
Do Ngoc Khanh <do.k.aa@m.titech.ac.jp>
2020-07-15 21:37:06 +0900

    Introduce function to simplify calc_beta_m.sh

--- monthly_temp/calc_beta_m.sh
+++ monthly_temp/calc_beta_m.sh
@@ -12,64 +12,42 @@ export GDAL_VRT_ENABLE_PYTHON=YES
 export PYTHONPATH=.
 export PYTHONSO=$HOME/miniconda3/lib/libpython3.7m.so
 
+compose_tif() {
+  local target_tif=$1
+  local python_function=$2
+  shift 2
+  local original_files=("$@")
+
+  if [[ -e "$target_tif" ]]; then
+    return
+  fi
+
+  local vrt_file=${target_tif%.*}.vrt
+  gdalbuildvrt "$vrt_file" "${original_files[@]}"
+  sed -i '/<VRTRasterBand/{
+    s/<VRTRasterBand/& subClass="VRTDerivedRasterBand"/
+    a <PixelFunctionLanguage>Python</PixelFunctionLanguage>
+    a <PixelFunctionType>'"$python_function"'</PixelFunctionType>
+  }' "$vrt_file"
+  gdal_translate -ot Float32 -co COMPRESS=LZW -co PREDICTOR=2 "$vrt_file" "$target_tif"
+  rm "$vrt_file"
+}
+
 moment=$1
 
 year_mean_tif="$moment"/temp_year_mean.tif
-
-if [[ ! -e "$year_mean_tif" ]]; then
-  vrt_file="$moment"/temp_year_mean.vrt
-  gdalbuildvrt "$vrt_file" "$moment"/??_tempmask.tif
-  sed -i '/<VRTRasterBand/{
-    s/<VRTRasterBand/& subClass="VRTDerivedRasterBand"/
-    a <PixelFunctionLanguage>Python</PixelFunctionLanguage>
-    a <PixelFunctionType>temperature.mean</PixelFunctionType>
-  }' "$vrt_file"
-  gdal_translate -ot Float32 -co COMPRESS=LZW -co PREDICTOR=2 "$vrt_file" "$year_mean_tif"
-  rm "$vrt_file"
-fi
+compose_tif "$year_mean_tif" "temperature.mean" "$moment"/??_tempmask.tif
 
 for month in {01..12}; do
-  vrt_file="$moment"/"$month"_alpha_m.vrt
   alpha_m="$moment"/"$month"_alpha_m.tif
-  if [[ -e "$alpha_m" ]]; then
-    continue
-  fi
-  gdalbuildvrt "$vrt_file" "$moment"/"$month"_tempmask.tif "$year_mean_tif"
-  sed -i '/<VRTRasterBand/{
-    s/<VRTRasterBand/& subClass="VRTDerivedRasterBand"/
-    a <PixelFunctionLanguage>Python</PixelFunctionLanguage>
-    a <PixelFunctionType>temperature.alpha_m</PixelFunctionType>
-  }' "$vrt_file"
-  gdal_translate -ot Float32 -co COMPRESS=LZW -co PREDICTOR=2 "$vrt_file" "$alpha_m"
-  rm "$vrt_file"
+  compose_tif "$alpha_m" "temperature.alpha_m" "$moment"/"$month"_tempmask.tif "$year_mean_tif"
 done
 
 alpha_m_mean="$moment"/alpha_m_mean.tif
-if [[ ! -e "$alpha_m_mean" ]]; then
-  vrt_file="$moment"/alpha_m_mean.vrt
-  gdalbuildvrt "$vrt_file" "$moment"/??_alpha_m.tif
-  sed -i '/<VRTRasterBand/{
-    s/<VRTRasterBand/& subClass="VRTDerivedRasterBand"/
-    a <PixelFunctionLanguage>Python</PixelFunctionLanguage>
-    a <PixelFunctionType>temperature.mean</PixelFunctionType>
-  }' "$vrt_file"
-  gdal_translate -ot Float32 -co COMPRESS=LZW -co PREDICTOR=2 "$vrt_file" "$alpha_m_mean"
-  rm "$vrt_file"
-fi
+compose_tif "$alpha_m_mean" "temperature.mean" "$moment"/??_alpha_m.tif
 
 for month in {01..12}; do
   alpha_m="$moment"/"$month"_alpha_m.tif
   beta_m="$moment"/"$month"_beta_m.tif
-  vrt_file="$moment"/"$month"_beta_m.vrt
-  if [[ -e "$beta_m" ]]; then
-    continue
-  fi
-  gdalbuildvrt "$vrt_file" "$alpha_m" "$alpha_m_mean"
-  sed -i '/<VRTRasterBand/{
-    s/<VRTRasterBand/& subClass="VRTDerivedRasterBand"/
-    a <PixelFunctionLanguage>Python</PixelFunctionLanguage>
-    a <PixelFunctionType>temperature.div</PixelFunctionType>
-  }' "$vrt_file"
-  gdal_translate -ot Float32 -co COMPRESS=LZW -co PREDICTOR=2 "$vrt_file" "$beta_m"
-  rm "$vrt_file"
+  compose_tif "$beta_m" "temperature.div" "$alpha_m" "$alpha_m_mean"
 done
