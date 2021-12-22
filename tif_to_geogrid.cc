ffff96d47849768377789319ec572bc104a6af8a
Do Ngoc Khanh <do.k.aa@m.titech.ac.jp>
2021-12-22 20:53:03 +0900

    Optimize wordsize. Change geogrid scale factor to 1e-2.

--- tif_to_geogrid.cc
+++ tif_to_geogrid.cc
@@ -1,10 +1,11 @@
 #include "gdal_priv.h"
+#include <algorithm>
 #include <cassert>
 #include <cstdio>
 
 int main(int argc, char **argv) {
-  if (argc != 26) {
-    printf("Usage: %s 00_utc_tif 01_utc_tif ... 24_utc_tif output_dir\n",
+  if (argc != 27) {
+    printf("Usage: %s 00_utc_tif 01_utc_tif ... 24_utc_tif output_dir_wordsize_2 output_dir_wordsize_3\n",
            argv[0]);
     return 0;
   }
@@ -26,14 +27,17 @@ int main(int argc, char **argv) {
 
   int col = bands[0]->GetXSize();
   int row = bands[0]->GetYSize();
-  const int tile_x = 1200;
+  const int tile_x = 600;
+
+  const int wordsize[] = {2, 3};
+  const char * output_dir[] = {argv[25], argv[26]};
 
   assert(col % tile_x == 0 && row % tile_x == 0);
   int tile_counts = (col / tile_x) * (row / tile_x);
   int tile_id = 0;
 
   int *data = new int[tile_x * tile_x * datasets.size()];
-  char *filename = new char[strlen(argv[25]) + 25];
+  char *filename = new char[std::max(strlen(output_dir[0]), strlen(output_dir[1])) + 25];
 
   for (int y_off = 0; y_off < row; y_off += tile_x) {
     for (int x_off = 0; x_off < col; x_off += tile_x) {
@@ -44,32 +48,37 @@ int main(int argc, char **argv) {
                                   data + tile_x * tile_x * i, tile_x, tile_x,
                                   GDT_Int32, 0, 0, nullptr) == CE_None);
       }
-      bool has_non_zero = false;
+      int max_value = 0;
       for (int i = 0; i < tile_x * tile_x * datasets.size(); ++i) {
+        data[i] /= 1000;
         if (data[i] < 0) {
           data[i] = 0;
         }
-        if (data[i] > 0) {
-          has_non_zero = true;
-        }
+        if (data[i] > max_value) max_value = data[i];
       }
-      if (!has_non_zero) {
-        continue;
-      }
+      if (max_value == 0) continue;
       int xstart = x_off + 1;
       int xend = xstart + tile_x - 1;
       int yend = row - y_off;
       int ystart = yend - tile_x + 1;
 
-      sprintf(filename, "%s/%05d-%05d.%05d-%05d", argv[25], xstart, xend,
-              ystart, yend);
+      int iwordsize = 0;
+      for (iwordsize = 0; iwordsize < 2; ++iwordsize) {
+        if (max_value < (1 << (8 * wordsize[iwordsize]))) {
+          break;
+        }
+      }
+      assert(iwordsize != 2);
+
+      sprintf(filename, "%s/%05d-%05d.%05d-%05d", output_dir[iwordsize], xstart,
+              xend, ystart, yend);
       FILE *fp = fopen(filename, "wb");
       for (int i = 0; i < bands.size(); ++i) {
         int *layer = data + tile_x * tile_x * i;
         for (int y = tile_x - 1; y >= 0; --y) {
           for (int x = 0; x < tile_x; ++x) {
             unsigned int v = layer[y * tile_x + x];
-            for (int shift = 24; shift >= 0; shift -= 8) {
+            for (int shift = 8 * (wordsize[iwordsize] - 1); shift >= 0; shift -= 8) {
               fputc(v >> shift & 0xff, fp);
             }
           }
@@ -79,8 +88,8 @@ int main(int argc, char **argv) {
     }
   }
 
-  {
-    sprintf(filename, "%s/index", argv[25]);
+  for (int iwordsize = 0; iwordsize < 2; ++iwordsize) {
+    sprintf(filename, "%s/index", output_dir[iwordsize]);
     FILE *fp = fopen(filename, "w");
     double padf[6];
     datasets[0]->GetGeoTransform(padf);
@@ -93,11 +102,11 @@ int main(int argc, char **argv) {
     fprintf(fp, "known_y = %d\n", row);
     fprintf(fp, "known_lat = %f\n", padf[3]);
     fprintf(fp, "known_lon = %f\n", padf[0]);
-    fprintf(fp, "wordsize = 4\n");
+    fprintf(fp, "wordsize = %d\n", wordsize[iwordsize]);
     fprintf(fp, "tile_x = %d\n", tile_x);
     fprintf(fp, "tile_y = %d\n", tile_x);
     fprintf(fp, "tile_z = %lu\n", bands.size());
-    fprintf(fp, "scale_factor = %e\n", 1e-5);
+    fprintf(fp, "scale_factor = %e\n", 1e-2);
     fprintf(fp, "missing_value = 0\n");
     fclose(fp);
   }
--- to_geogrid.sh
+++ to_geogrid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -l h_node=1
-#$ -l h_rt=00:20:00
+#$ -l h_rt=00:40:00
 
 . /etc/profile.d/modules.sh
 . "$HOME"/.bashrc
@@ -13,8 +13,14 @@ base_output_dir=$3
 
 for month in {01..12}; do
   output_dir=$base_output_dir/"$month"_24
+  output_dir_2_bytes="$output_dir"_2_bytes
+  output_dir_3_bytes="$output_dir"_3_bytes
   rm -rf "$output_dir"
   mkdir -p "$output_dir"
-  ./tif_to_geogrid.e "$basedir"/AHE_SSP3_"$year"_"$month"_??HR_UTC.tif "$output_dir" &
+  {
+    ./tif_to_geogrid.e "$basedir"/AHE_SSP3_"$year"_"$month"_??HR_UTC.tif "$output_dir_2_bytes" "$output_dir_3_bytes"
+    cd "$base_output_dir"
+    tar cfz "$month".tar.gz "$output_dir_2_bytes" "$output_dir_3_bytes"
+  } &
 done
 wait
